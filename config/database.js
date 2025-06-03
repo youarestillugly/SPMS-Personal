6f700bfaef8a8adddbe7c2705fb2e6bf5adbe36c
youarestillugly <justlittletoxic@gmail.com>
2025-06-03 11:30:25 +0600

    personal

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Needed for some hosted DBs with self-signed certs
+    },
+  },
+  logging: false, // disable SQL query logging, enable for debugging
+});
 
 module.exports = sequelize;
